da4b96212eb11e4611d20d239e1c5dee8df00b5f
nuboro <brorhultberg@gmail.com>
2016-12-19 20:59:10 +0100

    Create cg.py

---
+++ main/cg.py
@@ -0,0 +1,91 @@
+#    Copyright (C) 2016 Bror Hultberg
+
+#    This file is part of CG_module.
+
+#    CG_module is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    CG_module is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+class List:
+    def __init__(self,setname,taglist):
+        self.setname=setname 
+        self.taglist=taglist
+    def __str__(self):
+        return('LIST '+self.setname+' = '+self.taglist+' ;')
+
+
+class Setter:    
+    def __init__(self,setname,inlineset): 
+    	self.setname=setname 
+    	self.inlineset=inlineset
+    def __str__(self):
+        return('SET '+self.setname+' = '+self.inlineset+' ;')
+
+class Select:
+    def __init__(self,target,match):
+        self.target=target 
+        self.match=match 
+
+    def __str__(self):
+    	return("SELECT "+self.target+" IF "+self.match+' ;')
+
+class Remove:
+    def __init__(self,target,match):
+        self.target=target 
+        self.match=match 
+
+    def __str__(self):
+    	return("REMOVE "+self.target+" IF "+self.match+' ;')
+
+
+class Set(List,Setter):
+	pass
+class Rule(Select,Remove):
+    pass	 
+
+class CG(Rule,Set):
+
+    def __init__(self,delimiters,soft_delimiters,rules,sets):
+        self.delimiters=delimiters
+        self.soft_delimiters=soft_delimiters
+        self.rules=rules
+        self.sets=sets
+    def __str__(self):
+        print(self.delimiters)
+        print(self.soft_delimiters)
+        print('SETS')
+        for set in self.sets:
+            print(set)
+        print('SECTION')
+        for rule in self.rules:
+            print(rule)
+        return('SELECT (x);')
+
+
+set1 = List(setname="Pr", taglist="(pr)")
+set2 = List(setname="Adj", taglist="(adj)")
+set3 = Setter(setname="N", inlineset="(1 VERB)")
+
+rule1 = Select(target="N", match="(1 VERB)")
+rule2 = Select(target="N", match="(-1 ADJ)")
+rule3 = Remove(target="N", match="(1 VERB)")
+
+
+rules = [rule1, rule2, rule3]
+sets=[set1,set2,set3]
+cg = CG
+
+
+cg=CG('DELIMITERS = "<.>" "<!>" "<?>" "<...>" "<¶>" "<:>" ;','SOFT-DELIMITERS = "<,>" ;',rules,sets)
+print(cg)
