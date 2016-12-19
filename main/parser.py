b55eb088d72e9c82d25ecfcfcfed22ac1ea1cdcb
nuboro <brorhultberg@gmail.com>
2016-12-19 20:58:30 +0100

    Create parser.py

--- main/parser.py
+++ main/parser.py
@@ -1,12 +1,14 @@
 #    Copyright (C) 2016 Bror Hultberg
 #    Copyright (C) 2016 Joonas Kylmälä
 
-#    This program is free software: you can redistribute it and/or modify
+#    This file is part of CG_module.
+
+#    CG_module is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 
-#    This program is distributed in the hope that it will be useful,
+#    CG_module is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
