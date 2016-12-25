346120e82c1cb4f0b8d860da3b6f7f2fb611e247
Joonas Kylmälä <j.kylmala@gmail.com>
2016-12-25 21:19:34 +0200

    add missing imports

--- main/parser.py
+++ main/parser.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import math
+import collections
 import fileinput
 from streamparser import parse_file
 
