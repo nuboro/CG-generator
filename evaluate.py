8866d7d0f93aa70b03fe6b15badb59ef2398f2b4
nuboro <brorhultberg@gmail.com>
2017-01-15 19:39:05 +0100

    improved evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -19,7 +19,6 @@
 
 
 from streamparser import parse
-from parser import wordclass
 import subprocess
 import collections
 
@@ -69,15 +68,12 @@ def average_ambiguity(option):  #add lambda function
         i=i+1
     return(ambiguity/i)
 
-    
-def ambiguity_original(text):
-    original = subprocess.getoutput('(cd /home/bror/apertium/languages/apertium-kaz/; echo "'+text+'" | lt-proc kaz.automorf.bin |cg-conv -a | vislcg3 --trace -g apertium-kaz.kaz.rlx |cg-conv -A)')
-    return(average_ambiguity(original))
 
 
-def ambiguity_induced(text, induced):
-    induced = subprocess.getoutput('(cd /home/bror/apertium/languages/apertium-kaz/; echo "'+text+'" | lt-proc kaz.automorf.bin |cg-conv -a | vislcg3 --trace -g' + induced + '|cg-conv -A)')
-    return(average_ambiguity(induced))
+
+def ambiguity_option(text, option):
+    analysed = subprocess.getoutput('(cd /home/bror/apertium/languages/apertium-kaz/; echo "'+text+'" | lt-proc kaz.automorf.bin |cg-conv -a |' + option +'cg-conv -A)')
+    return(average_ambiguity(analysed))
 
 
 def ambiguity_handtagged(handtagged):
@@ -86,20 +82,33 @@ def ambiguity_handtagged(handtagged):
 
 def count_right_analysis(zip_morphed_vs_handtagged):
     count=0
+    total=0
     for morphed, handtag in zip_morphed_vs_handtagged:
+        total=total+1
         if (morphed.wordform, morphed.knownness, morphed.readings) == (handtag.wordform, handtag.knownness, handtag.readings):
             count=count+1
-    return(count)
+    return(count/total)
+
+#(cd /home/bror/apertium/langum-kaz/; echo "text" | lt-proc kaz.automorf.bin |cg-conv -a | vislcg3 --trace -g apertium-kaz.kaz.rlx | vislcg3 --trace -g apertium-kaz.kaz.rlx |cg-conv -A)
+
 
 
 def best_ambiguity(ambiguities):
     best = 999999999999999
-    index = -1
+    treshold = -1
     for i in range(len(ambiguities)):
         if best > ambiguities[i][1]:
             best = ambiguities[i][1]
-            index = i
-    return(best, i)
+            treshold = ambiguities [i] [0]
+    return(best, treshold)
+
+
+def count_rules(file):
+    with open(file, 'r') as data:
+        plaintext = data.read()
+    count=plaintext.count("\nSELECT")
+    count= count+plaintext.count("\nREMOVE")
+    return(count)
 
 
 
@@ -124,33 +133,62 @@ for i in range(10):
 
     ambiguity_on_treshold = []
     tresh = 0.00
-    while tresh <= 0.15: 
+    while tresh < 0.15: 
         tresh = tresh + 0.01
         treshold = collections.namedtuple("treshold_and_ambinguity", 'treshold ambiguity_induced')
-        cg_file = subprocess.check_output(["python3",'main.py', "/tmp/TrainingData"+str(i), "-t "+ str(tresh)], universal_newlines=True)
-        with open("/tmp/Induced_Rules"+str(i), 'w') as write:
+        cg_file = subprocess.check_output(["python3",'main.py', "/tmp/TrainingData"+str(i), "-t "+ str(tresh)], universal_newlines=True, cwd='main/')
+        with open("/tmp/Induced_Rules"+str(i)+"tres"+str(tresh), 'w') as write:
             write.write(cg_file)
-        morphed_induced = subprocess.getoutput('(cd /home/bror/apertium/languages/apertium-kaz/; echo "' + text[i] + '" | lt-proc kaz.automorf.bin |cg-conv -a | vislcg3 --trace -g '+"/tmp/Induced_Rules"+str(i)+'|cg-conv -A)')
+        morphed_induced = subprocess.getoutput('(cd /home/bror/apertium/languages/apertium-kaz/; echo "' + text[i] + '" | lt-proc kaz.automorf.bin |cg-conv -a | vislcg3 --trace -g '+"/tmp/Induced_Rules"+str(i)+"tres"+str(tresh)+'|cg-conv -A)')
         morphed_induced = parse(c for c in morphed_induced)
         handtagged2 = parse(c for c in data[i])
         induced_vs_handtagged = zip(morphed_induced, handtagged2)
-        ambinguity = ambiguity_induced(text[i], "/tmp/Induced_Rules"+str(i) )
+        ambinguity = ambiguity_option(text[i], "vislcg3 --trace -g /tmp/Induced_Rules"+str(i)+"tres"+str(tresh)+"|" )
 
         ambiguity_on_treshold.append(treshold(treshold= tresh, ambiguity_induced= ambinguity))
 
    
     best_average_ambiguity = (best_ambiguity(ambiguity_on_treshold))
 
-    print("EVALUATE ON TEXT " + str(i))
-    print("AVERAGE AMBIGUITY ON THE ORIGINAL RULES " + str(ambiguity_original(text[i])))
-    print("BEST AVERAGE AMBIGUITY ON THE INDUCED RULES TRESHOLD " + str(best_average_ambiguity[1]) + " AVERAGE " + str(best_average_ambiguity[0]))
-    print("AVERAGE AMBIGUITY ON THE HANDTAGGED CORPUS " + str(ambiguity_handtagged(data[i])))
-    print("ORIGINAL RULES")
-    print(count_right_analysis(morphed_vs_handtagged))
-    print("================================================")
-    print("INDUCED RULES")
-    print(count_right_analysis(induced_vs_handtagged))
+    best_treshold = best_average_ambiguity[1]
+
+    handtagged3 = parse(c for c in data[i])
+    handtagged4 = parse(c for c in data[i])
+    morphed_origal_than_induced = parse(c for c in subprocess.getoutput('(cd /home/bror/apertium/languages/apertium-kaz/; echo "'+text[i]+'" | lt-proc kaz.automorf.bin |cg-conv -a | vislcg3 --trace -g apertium-kaz.kaz.rlx | vislcg3 --trace -g /tmp/Induced_Rules' +str(i)+"tres"+str(best_treshold)+' |cg-conv -A)'))
+    morphed_induced_than_original = parse(c for c in subprocess.getoutput('(cd /home/bror/apertium/languages/apertium-kaz/; echo "'+text[i]+'" | lt-proc kaz.automorf.bin |cg-conv -a | vislcg3 --trace -g /tmp/Induced_Rules' +str(i)+"tres"+str(best_treshold) +'| vislcg3 --trace -g apertium-kaz.kaz.rlx |cg-conv -A)'))
+
+    induced_than_original_vs_handtagged = zip(morphed_induced_than_original, handtagged3)
+    original_than_induced_vs_handtagged = zip(morphed_origal_than_induced, handtagged4)
+
+
+    print("Split | Avg. ambig | (H) N. rules  | (I) N. rules | (H) Av. ambig | (H) Acc. | (I) Av. ambig | (I) Acc. | (HI) Av. ambig | (HI) Acc. | (IH) Av. ambig | (IH) Acc. |")
+
+    print( str(i)+"| ", end="")
+    print(str(ambiguity_option(text[i], ''))+"| ", end="")
+    print(str(count_rules('/home/bror/apertium/languages/apertium-kaz/apertium-kaz.kaz.rlx'))+"| ", end="")
+
+    print(str(count_rules('/tmp/Induced_Rules' +str(i)+"tres"+str(best_treshold)))+"| ", end="")
+    print(str(ambiguity_option(text[i], 'vislcg3 --trace -g apertium-kaz.kaz.rlx |'))+"| ", end="")
+    print(str(count_right_analysis(morphed_vs_handtagged))+"| ", end="")
+    print(str(best_average_ambiguity[0])+"| ", end="")
+    print(str(count_right_analysis(induced_vs_handtagged))+"| ", end="")
+
+
+    print(str(ambiguity_option(text[i], 'vislcg3 --trace -g apertium-kaz.kaz.rlx | vislcg3 --trace -g /tmp/Induced_Rules' +str(i)+"tres"+str(best_treshold)+"|"))+"| ", end="")
+    print(str(count_right_analysis(original_than_induced_vs_handtagged))+"| ", end="")
+    print(str(ambiguity_option(text[i], 'vislcg3 --trace -g /tmp/Induced_Rules' +str(i)+"tres"+str(best_treshold) +'| vislcg3 --trace -g apertium-kaz.kaz.rlx |'))+"| ", end="")
+
+
+
+
+
+    print(str(count_right_analysis(induced_than_original_vs_handtagged))+"| ", end="")
+
+
+
     print("\n")
+
+
 
 
 
